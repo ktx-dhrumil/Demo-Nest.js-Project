340d4c1da8431b9fd0153f885d9c6c7adc6766d6
DhrumilTrada <dhrumiltrada11@gmail.com>
2025-07-17 13:25:44 +0530

    chore: changes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
-  console.log("Server started on", process.env.PORT || 3000)
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  await app.init();
 }
+
 bootstrap();
+
+export default server;
